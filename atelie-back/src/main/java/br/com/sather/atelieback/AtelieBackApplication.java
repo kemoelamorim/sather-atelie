0af034feb712b544462889ce3046371d9ba054f2
Kemoel Amorim Miranda <kemoel.amorim@workdb.com.br>
2023-03-06 16:25:17 -0300

    create repository

---
+++ atelie-back/src/main/java/br/com/sather/atelieback/AtelieBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.sather.atelieback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtelieBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtelieBackApplication.class, args);
+	}
+
+}
---
+++ atelie-back/src/test/java/br/com/sather/atelieback/AtelieBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sather.atelieback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtelieBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
